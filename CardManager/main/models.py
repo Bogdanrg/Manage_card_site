4d26b465ca0484580c0ee4b507ab790abff1af97
Bogdanrg <70534359+Bogdanrg@users.noreply.github.com>
2022-08-19 00:18:29 +0300

    Completed

--- CardManager/main/models.py
+++ CardManager/main/models.py
@@ -1,20 +1,20 @@
-from django.db import models
+from django.db import models, connection
 from django.urls import reverse
 import datetime
+from random import randrange
 
 
-def expiration_six_month():
-    date_now = datetime.date.today()
-    month = date_now.month
-    date_expiration = date_now.replace(month=month-1)
-    return date_expiration
+def expiration_date_func(days):
+    date_now = datetime.datetime.now()
+    expiration_date = date_now + datetime.timedelta(days=days)
+    return expiration_date
 
 
 class Card(models.Model):
     series = models.CharField(max_length=30)
     number = models.IntegerField()
-    release_date = models.DateField(default=datetime.date.today)
-    expiration_date = models.DateField(default=expiration_six_month)
+    release_date = models.DateTimeField(default=datetime.datetime.now())
+    expiration_date = models.DateTimeField()
     date_of_use = models.DateTimeField(auto_now=True)
     count = models.IntegerField(null=True)
     status = models.BooleanField()
@@ -29,6 +29,14 @@ class Card(models.Model):
         return reverse('delete', kwargs={'card_id': self.pk})
 
 
+def create_cards():
+    for obj in range(10):
+        with connection.cursor() as cursor:
+            cursor.execute("INSERT INTO main_card(series, number, release_date, expiration_date, date_of_use, count,"
+                           " status) VALUES('first', %s,'2022-08-19 00:00:00' , %s, '2022-08-19 00:00:00', %s, 1)",
+                           [randrange(10101010, 98989898), expiration_date_func(days=365), 300])
+
+
 class Purchase(models.Model):
     APPlE = 'APPLE'
     ORANGE = 'ORANGE'
@@ -40,12 +48,3 @@ class Purchase(models.Model):
     price = models.IntegerField()
     buy_date = models.DateTimeField(auto_now=True)
     card_id = models.ForeignKey('Card', on_delete=models.CASCADE)
-
-#for i in range(10):
-...     #dict={}
-...     #dict['series'] = 'third'
-...     #dict['number'] = randrange(10101010, 98989898)
-...     #dict['count'] = randrange(10, 500)
-...     #dict['status'] = True
-...     #Card.objects.create(**dict)
-#
